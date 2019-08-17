6cb9ee03c9eca766d20cc5bc4f993415955095f3
Gideon Mwega <mwegagideon@gmail.com>
2019-08-17 22:11:22 +0300

    adding file changes before transferring

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <v-layout id="app" row wrap>
     <v-flex xs12 sm10 offset-sm1 fill-height>
-      <v-card-text class="time-right"> {{this.getTime }} </v-card-text>
+      <v-card-text class="time-right">{{this.getTime }}</v-card-text>
       <v-card class="progress-bar">
-      <v-card-title class="app-title">***Last-Man Standing***</v-card-title>
-      <v-spacer></v-spacer>
+        <v-card-title class="app-title">***Last-Man Standing***</v-card-title>
+        <v-spacer></v-spacer>
 
-      <h4 class="text">$ Defeat the Boss to advance $</h4>
-      <v-spacer></v-spacer>
-      
-        <v-card-text >Total_session_time: {{ countDownBarStart }} ms.</v-card-text>
+        <h4 class="text">$ Defeat the Boss to advance $</h4>
+        <v-spacer></v-spacer>
 
+        <v-card-text>Total_session_time: {{ countDownBarStart }} sec.</v-card-text>
       </v-card>
       <v-spacer></v-spacer>
       <v-card>
-        <v-card-text > {{this.countDown }} </v-card-text>
+        <v-card-text >
+          <span> {{ this.countDown }}</span> <br />
 
+          <v-btn flat color="blue"> Begin Game</v-btn>
+        </v-card-text>
       </v-card>
-      
+
       <Playground />
     </v-flex>
   </v-layout>
@@ -34,25 +36,28 @@ export default {
   data() {
     return {
       // implement countdown with js
-      countDownBarStart: "7200"
+      countDownBarStart: "120"
     };
   },
-  computed:{
-    getTime: function () {
-      let localTime = Date().toLocaleLowerCase();
+  computed: {
+    getTime: function() {
+      let current = new Date();
+      let localTime = current.toDateString();
       return localTime;
     },
-    countDown: function () {
+    countDown: function() {
       let startValue = this.countDownBarStart;
 
-      let newValue =Date().toLocaleLowerCase();
-      return {
-        startValue,
-        newValue
-      };
+      let newValue = parseInt(startValue);
 
+      let limit = 0;
 
-      
+      while (newValue >= limit) {
+        let deduct = newValue--;
+        console.log(deduct);
+        return deduct;
+      } 
+
     }
   }
 };
@@ -81,7 +86,7 @@ export default {
   padding-bottom: 30px;
   font-size: 1.3em;
 }
-.time-right{
+.time-right {
   text-align: end;
 }
 </style>
